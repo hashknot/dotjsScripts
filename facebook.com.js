6faa32dfdc9ef32070644fd6389d9a81f5533d67
Jitesh <msg.jitesh@gmail.com>
2012-10-31 01:10:12 +0530

    Made the Facebook Blue Title Bar Scrollable.

--- facebook.com.js
+++ facebook.com.js
@@ -1,30 +1,38 @@
 var count=1;
 function removeAds(){
-	console.log("Removing ads");
-	ads = document.getElementsByClassName('ego_column');
-	if(ads == null){
-		setTimeout(arguments.callee,count * 500);
-		count++;
-		if (count > 1000000) 
-			count = 100;
-		return;
-	}	
-	for(var i=0;i<ads.length;i++){
-		var ad = ads[i];
-		if(ad.className == 'ego_column'){
-			var baap = ads[i].parentNode; //baap means Father in Marathi
-			baap.removeChild(ads[i]);	//Baap to Child : You are banished forever, GET LOST YOU BASTARD
-		}
-	}
-	document.addEventListener("DOMNodeInserted",removeAgain , false) //I have my eyes on you. If you DARE come back, I will KICK YOUR ASS again.
-}
+      console.log("Removing ads");
+      ads = document.getElementsByClassName('ego_column');
+      if(ads == null){
+	 setTimeout(arguments.callee,count * 500);
+	 count++;
+	 if (count > 1000000) 
+	 count = 100;
+	 return;
+      }	
+      for(var i=0;i<ads.length;i++){
+	 var ad = ads[i];
+	 if(ad.className == 'ego_column'){
+	    var baap = ads[i].parentNode; 	//baap means Father in Marathi
+	    baap.removeChild(ads[i]);	//Baap to Child : You are banished forever, GET LOST YOU BASTARD
+	 }
+      }
+      document.addEventListener("DOMNodeInserted",removeAgain , false) //I have my eyes on you. If you DARE come back, I will KICK YOUR ASS again.
+   }
 
-function removeAgain(){
-	if(typeof timeoutID == "number") {
-		window.clearTimeout(timeoutID);
-		timeoutID = null;
-	}
-	timeoutID = window.setTimeout(removeAds , 700);
-}
+   function removeAgain(){
+      if(typeof timeoutID == "number") {
+	 window.clearTimeout(timeoutID);
+	 timeoutID = null;
+      }
+      timeoutID = window.setTimeout(removeAds , 700);
+   }
 
-removeAds(); 
+
+   function editTitleBar(){
+      console.log("Making Bluebar scrollable");
+      blueBar = document.getElementById('blueBar');
+      blueBar.className = "";
+   }
+
+   removeAds(); 
+   editTitleBar(); //Remove the Fixed Property of Upper Blue bar of Facebook.
